cc0ad6a6a69b7fa5ba2d8fc1a07ca897643c9560
StinaG <44798361+StinaG@users.noreply.github.com>
2019-05-23 22:54:33 +0200

    Script inlagt, fixat boxshadow mm

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('.btnspel1').click(function(event){
+		$('#kort1').toggle();
+	});
+});
+
+$(function(){
+	$('.btnspel2').click(function(event){
+		$('#kort2').toggle();
+	});
+});
+$(function(){
+	$('.btnspel3').click(function(event){
+		$('#kort3').toggle();
+	});
+});
